b5bfeae30a4861306283c142aa3596b19888ae00
Christian <christian.wrona@gmail.com>
2022-01-21 15:48:26 -0500

    Chapter 7 complete

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from rango.models import Category, Page
+from rango.forms import CategoryForm
 
 def index(request):
     category_list=Category.objects.order_by('-likes')[:5]
@@ -11,24 +12,32 @@ def index(request):
     context_dict['pages']=pages_list
     return render(request, 'rango/index.html', context=context_dict)
 
+
 def about(request):
     context_dict = {'boldmessage':'This tutorial has been put together by kischin.'}
     return render(request, 'rango/about.html', context=context_dict)
 
+
 def show_category(request, category_name_slug):
     context_dict = {}
-
     try:
         category = Category.objects.get(slug=category_name_slug)
-
         pages = Page.objects.filter(category=category)
-
         context_dict['pages'] = pages
         context_dict['category'] = category
-
     except:
         context_dict['pages'] = None
         context_dict['category'] = None
-
-
     return render(request, 'rango/category.html', context=context_dict)
+
+
+def add_category(request):
+    form = CategoryForm()
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return redirect('/rango/')
+        else:
+            print(form.errors)
+    return render(request, 'rango/add_category.html', {'form': form})
