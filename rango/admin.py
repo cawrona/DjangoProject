85ac239cb2924cc61a7a95cec9e1464fdc1b75f2
Christian <christian.wrona@gmail.com>
2022-01-31 00:20:34 -0500

    Chapter 9.5 complete

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from rango.models import Category, Page
+from rango.models import Category, Page, UserProfile
 
 # Register your models here.
 
@@ -15,3 +15,4 @@ class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name',)}
 
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(UserProfile)
