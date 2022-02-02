dfc532c38f0e003b3f1a31b9b4ac7193265056f4
Christian <christian.wrona@gmail.com>
2022-02-01 23:54:50 -0500

    Chapter 9.7 complete

--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from django.urls import reverse
-
+from django.contrib.auth import authenticate, login
 
 def index(request):
     category_list=Category.objects.order_by('-likes')[:5]
@@ -96,6 +96,26 @@ def register(request):
         request,
         'rango/register.html',
         context={'user_form': user_form,
+                 'profile_form': profile_form,
                  'registered': registered}
     )
 
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return redirect(reverse('rango:index'))
+            else:
+                return HttpResponse("Your Rango account is disabled.")
+        else:
+            print(f'Invalid login details: {username}, {password}')
+            return HttpResponse("Invalid login details supplied.")
+    else:
+        return render(request, 'rango/login.html')
